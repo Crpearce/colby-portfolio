5627112de52e6819e198e2e6ef37b1f84ecf915b
Colby Pearce <crpearce892@gmail.com>
2023-02-01 15:11:48 -0700

    update page with media queries and initial css styles for index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import Navbar from "./sections/navbar/navbar.component";
+import Header from "./sections/header/header.component"
+import About from './sections/about/about.component';
+import Services from './sections/services/services.component';
+import Portfolio from "./sections/portfolio/portfolio.component";
+import Testimonials from "./sections/testimonials/testimonials.component";
+import FAQs from './sections/faqs/faqs.component';
+import Contact from './sections/contact/contact.component';
+import Footer from './sections/footer/footer.component';
+import FloatingNav from './sections/floatingNav/floatingNav.component';
+
 const App = () => {
     return (
-        <div>App</div>
+        <main>
+            <Navbar />
+            <Header />
+            <About />
+            <Services />
+            <Portfolio />
+            <Testimonials />
+            <FAQs />
+            <Contact />
+            <Footer />
+            <FloatingNav />
+        </main>
     );
 }
 
