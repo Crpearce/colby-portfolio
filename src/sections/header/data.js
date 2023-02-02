244e1ba75eba8ead72d793936130ab72d5979a7a
Colby Pearce <crpearce892@gmail.com>
2023-02-02 09:58:46 -0700

    update header and nav

---
+++ src/sections/header/data.js
@@ -0,0 +1,13 @@
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {AiFillGithub} from 'react-icons/ai'
+import {MdComputer} from 'react-icons/md'
+
+
+
+const data = [
+    {id: 1, link: 'https://www.linkedin.com/in/colby-pearce1/', icon: <AiOutlineLinkedin/>},
+    {id: 2, link: 'https://github.com/Crpearce', icon: <AiFillGithub/>},
+    {id: 3, link: 'https://terminal.turing.edu/profiles/1596', icon: <MdComputer/>}
+]
+
+export default data
--- src/sections/navbar/navbar.component.jsx
+++ src/sections/navbar/navbar.component.jsx
@@ -1,15 +1,25 @@
+import Logo from "../../assets/name-logo.png";
+import data from "./data";
+
+import {AiOutlineBgColors} from 'react-icons/ai';
+
 import "./navbar.styles.css";
-import Logo from '../../assets/name-logo.png'
 
 const Navbar = () => {
   return (
     <nav>
-      <div className="container nav__container">
-        <a href="">
-          <img src={Logo} alt="working?" />
+      <div className="container nav-container">
+        <a href="index.html" className="nav-logo">
+          <img src={Logo} alt="Logo" />
         </a>
-        <ul></ul>
-        <button></button>
+        <ul className="nav-menu">
+          {data.map((category) => (
+            <li key={category.id}>
+              <a href={category.link}>{category.title}</a>
+            </li>
+          ))}
+        </ul>
+        <button id='theme-icon'><AiOutlineBgColors /></button>
       </div>
     </nav>
   );
