a069eaedee3c158caac5f4a13fc194743be2f3c8
Colby Pearce <crpearce892@gmail.com>
2023-02-14 15:47:06 -0700

    edit contact text color

--- src/sections/contact/contact.component.jsx
+++ src/sections/contact/contact.component.jsx
@@ -4,7 +4,7 @@ import './contact.styles.css'
 const Contact = () => {
   return (
     <section id="contact">
-      <h2>Get In Touch</h2>
+      <h2 className='in-touch'>Get In Touch</h2>
       <p>
       I'm passionate about technology and feel that I have something to offer the industry. I look forward to taking a junior position with a company that will allow me to continue to learn new technologies while building upon my existing strengths in a team environment.
       </p>
